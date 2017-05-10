124c74b4062be18a5a62211effda7b035f4a2609
Alex Collins <acollins@coherent.net.au>
2017-05-10 11:57:33 +1000

    Implemented RayCasting for picking up pieces

--- Assets/Scripts/CheckerBoard.cs
+++ Assets/Scripts/CheckerBoard.cs
@@ -2,15 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckerBoard : MonoBehaviour {
+public class CheckerBoard : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject blackPiece;
+    public GameObject whitePiece;
+
+    public int boardX = 8, boardZ = 8;
+    public float pieceRadius = 0.5f;
+
+    public Piece[,] pieces;
+
+    private int halfBoardX, halfBoradZ;
+    private float pieceDiameter;
+    private Vector3 bottomLeft;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // calculate a few values
+        halfBoardX = boardX / 2;
+        halfBoradZ = boardZ / 2;
+        pieceDiameter = pieceRadius * 2;
+        bottomLeft = transform.position - Vector3.right * halfBoardX - Vector3.forward * halfBoradZ;
+
+        CreateGrid();
+
+    }
+
+    void CreateGrid()
+    {
+        // initialise 2D array 
+        pieces = new Piece[boardX, boardZ];
+
+        #region Generate white pieces 
+        // loop trhrough board columns and skip 2 each time 
+        for (int x = 0; x < boardX; x += 2)
+        {
+            // loop through first 3 rows
+            for (int z = 0; z < 3; z++)
+            {
+                // check even row - if its not even must be odd
+                bool evenRow = z % 2 == 0;
+                // generate piece 
+                int gridX = evenRow ? x : x + 1;
+                int gridZ = z;
+                // generate piece
+                GeneratePiece(whitePiece, gridX, gridZ);
+            }
+
+        }
+        #endregion
+
+        #region Generate Black Pieces
+        for (int x = 0; x < boardX; x += 2)
+        {
+            // loop through first 3 rows
+            for (int z = 5; z < 8; z++)
+            {
+                // check even row - if its not even must be odd
+                bool evenRow = z % 2 == 0;
+                // generate piece 
+                int gridX = evenRow ? x : x + 1;
+                int gridZ = z;
+                // generate piece
+                GeneratePiece(blackPiece, gridX, gridZ);
+                
+            }
+        }
+        #endregion
+
+    }
+
+    void GeneratePiece(GameObject piecePrefab, int x, int z)
+    {
+
+        // Crate instance of piece
+        GameObject clone = Instantiate(piecePrefab);
+        // set the parent to be this transform 
+        clone.transform.SetParent(transform);
+        // get the piece from the clone 
+        Piece piece = clone.GetComponent<Piece>();
+        // place the piece
+        PlacePiece(piece, x, z);
+
+    }
+
+    void PlacePiece(Piece piece, int x, int z)
+    {
+        float xOffset = x * pieceDiameter + pieceRadius;
+        float zOffset = z * pieceDiameter + pieceRadius;
+        // set pieces new grid coordinates 
+        piece.gridX = x;
+        piece.gridZ = z;
+        // move physicall to board coordinates
+        piece.transform.position = bottomLeft + Vector3.right * xOffset + Vector3.forward * zOffset;
+        // set pieces array slot
+        pieces[x, z] = piece;
+
+    }
+
+
 }
---
+++ Assets/Scripts/PiecePicker.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GGL;
+
+
+public class PiecePicker : MonoBehaviour
+{
+
+    public float pieceHeight = 5f;
+    public float rayDistance = 1000f;
+    public LayerMask selectionIgnoreLayer;
+
+    private Piece selectedPiece;
+    private CheckerBoard board;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // find the checkerboard in the scene
+        board = FindObjectOfType<CheckerBoard>();
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+        CheckSelection();
+        MoveSelection();
+
+    }
+
+    void MoveSelection()
+    {
+        // check if we have a piece seleeted
+        // if not = 0 we have one selected
+        if (selectedPiece != null)
+        {
+            // create new ray from camera 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance, 0.1f, 0.1f, Color.yellow, Color.yellow);
+
+            RaycastHit hit;
+            // Raycast to only hit objects that arent pieces 
+            if (Physics.Raycast(ray, out hit, rayDistance, ~selectionIgnoreLayer))
+
+            {
+                // obtain hit point
+                // GizmosGL.color = Color.red;
+                GizmosGL.AddSphere(hit.point, 0.5f);
+                // move the piece to position
+                Vector3 piecePos = hit.point + Vector3.up * pieceHeight;
+                selectedPiece.transform.position = piecePos;
+
+            }
+
+        }
+    }
+
+
+
+    void CheckSelection()
+    {
+        // create a ray from camera mouse position to world
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance);
+
+        RaycastHit hit;
+        // Check if the player hits the mouse button
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Cast a ray 
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                // set the selected piece to be the hit object
+                selectedPiece = hit.collider.GetComponent<Piece>();
+                // check if the user did not hit a piece
+                if (selectedPiece == null)
+                {
+                    // display message 
+                    Debug.Log("cannot pick up Object: " + hit.collider.name);
+                }
+
+            }
+        }
+    }
+}
